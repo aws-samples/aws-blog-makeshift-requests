1f9e5af8971a157a96d9494a1949b5e23ccb0b25
sanket.sirsikar-tc <sanket.sirsikar@toyotaconnected.com>
2020-07-25 18:55:31 -0500

    initial commit

---
+++ makeshift-requests-api/src/main/java/com/dataanalytics/awsblogs/StepRequestData.java
@@ -0,0 +1,24 @@
+package com.dataanalytics.awsblogs;
+
+public class StepRequestData {
+
+    private String cognitoId;
+    private String requestId;
+
+    public String getCognitoId() {
+        return cognitoId;
+    }
+
+    public void setCognitoId(String cognitoId) {
+        this.cognitoId = cognitoId;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+}
